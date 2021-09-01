f6e2ec81c224ab85034d5628bd887381fa663d10
edvsc779 <edvsc779@tlvm-5-3-3.ad.liu.se>
2021-09-01 16:28:16 +0200

    Startat lite

--- life/src/life.cpp
+++ life/src/life.cpp
@@ -6,10 +6,22 @@
 #include "grid.h"
 #include "lifeutil.h"
 #include <string>
+
+using namespace std;
+
+string askUserForFileName() {
+    string x;
+    cout << "Enter your filename! " << endl;
+    cin >> x;
+    cout << "Filename: " << x << endl;
+    return x;
+}
+
 int main() {
 
     // TODO: Finish the program!
 
-    std::cout << "Have a nice Life! "  << std::endl;
+    cout << "Have a nice Life! "  << endl;
+    askUserForFileName();
     return 0;
 }
