be4477d26c3ba929e1532736574e9888bec3187f
edvsc779 <edvsc779@su04-106.ad.liu.se>
2021-09-07 11:37:39 +0200

    Färdig

--- life/src/life.cpp
+++ life/src/life.cpp
@@ -11,11 +11,17 @@
 using namespace std;
 
 string askUserForFileName() {
-    string x;
-    cout << "Enter your filename! " << endl;
-    cin >> x;
-    cout << "Filename: " << x << endl;
-    return x;
+    string str;
+    cout << "Grid input file name? ";
+    cin >> str;
+    return str;
+}
+
+string askUserForAction() {
+    string str;
+    cout << "a)nimate, t)ick, q)uit? ";
+    cin >> str;
+    return str;
 }
 
 int convertStringToInt(string str) {
@@ -36,8 +42,7 @@ void setGrid(Grid<char>& grid, ifstream& input, string& line, int& row_length,
 }
 
 void getGridFromFile(Grid<char>& grid) {
-    string filename ="simple.txt"; //ask_user_for_filename()
-    //string filename = askUserForFileName();
+    string filename = askUserForFileName();
     ifstream input;
     input.open(filename.c_str());
     string line;
@@ -51,7 +56,6 @@ void getGridFromFile(Grid<char>& grid) {
     grid.resize(row_length, col_length);
     setGrid(grid, input, line, row_length, col_length);
 
-    cout << grid.toString() << endl;
     input.close();
 }
 
@@ -68,47 +72,87 @@ int checkTotalNeighbours(Grid<char>& grid, int row, int col) {
     int totalNeighbours = 0;
 
     for (int inner_row = row-1; inner_row < row+2; inner_row++) {
-        for (int inner_col = col-1; inner_col < col+2; inner_col++) {
 
+        for (int inner_col = col-1; inner_col < col+2; inner_col++) {
             if (grid.inBounds(inner_row, inner_col)) {
 
-                if (grid.get(inner_row, inner_col) == 'X') { // and if inte 0,0
-                    if (inner_row != row && inner_col != col) {
+                if (grid.get(inner_row, inner_col) == 'X') {
+
+                    if ((inner_row != row) || (inner_col != col)) {
                         totalNeighbours++;
                     }
                 }
             }
         }
     }
-
-    cout << totalNeighbours << endl;
     return totalNeighbours;
 }
 
 void advanceGrid(Grid<char>& grid) {
+    Grid<char> tempGrid(grid.numRows(), grid.numCols());
+    int neighbours = 0;
+
     for(int row=0; row < grid.numRows(); row++) {
         for(int col=0; col < grid.numCols(); col++) {
-            char cellType = grid.get(row, col);
-            int neighbours = checkTotalNeighbours(grid, row, col);
+            neighbours = checkTotalNeighbours(grid, row, col);
 
+            if (neighbours <= 1) {
+                tempGrid.set(row, col, '-');
+            }
+
+            else if (neighbours == 2) {
+                tempGrid.set(row, col, grid.get(row, col));
+            }
+
+            else if (neighbours == 3) {
+                tempGrid.set(row, col, 'X');
+            }
+
+            else {
+                tempGrid.set(row, col, '-');
+            }
         }
+    }
+    grid = tempGrid;
+}
+
+void animate(Grid<char>& grid) {
+    int generations = 0;
+    while(1==1) {
+        advanceGrid(grid);
+        generations++;
+        clearConsole();
+        printGrid(grid);
+        cout << "generations: " << generations << endl;
+        pause(100);
     }
 }
 
+void userInterface(Grid<char>& grid) {
+    string action = askUserForAction();
+    if (action == "a") {
+        animate(grid);
+    }
+
+    else if (action == "t") {
+        advanceGrid(grid);
+        printGrid(grid);
+        userInterface(grid);
+    }
+
+    else if (action == "q") {
+        cout << "Have a nice Life! "  << endl;
+    }
+
+    else {
+        userInterface(grid);
+    }
+}
 
 int main() {
-
-    // TODO: Finish the program!
-
-    cout << "Have a nice Life! "  << endl;
-
     Grid<char> grid;
     getGridFromFile(grid);
-    cout << endl;
     printGrid(grid);
-    checkTotalNeighbours(grid, 0,0);
-    cout << "done...";
+    userInterface(grid);
     return 0;
-
-
 }
