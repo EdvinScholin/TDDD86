2e416463b3b97b00bd1a9eace22710c869f6eecb
edvsc779 <edvsc779@su04-106.ad.liu.se>
2021-09-07 12:21:38 +0200

    TDDD86 Lab 1 redovisning

--- life/src/life.cpp
+++ life/src/life.cpp
@@ -1,7 +1,3 @@
-// This is the CPP file you will edit and turn in.
-// Also remove these comments here and add your own.
-// TODO: remove this comment header
-
 #include <iostream>
 #include "grid.h"
 #include "lifeutil.h"
@@ -10,6 +6,9 @@
 #include <sstream>
 using namespace std;
 
+/*
+ * Asks for a start colony in text format.
+ */
 string askUserForFileName() {
     string str;
     cout << "Grid input file name? ";
@@ -17,6 +16,9 @@ string askUserForFileName() {
     return str;
 }
 
+/*
+ * Asks user for what should be done in the program.
+ */
 string askUserForAction() {
     string str;
     cout << "a)nimate, t)ick, q)uit? ";
@@ -24,15 +26,21 @@ string askUserForAction() {
     return str;
 }
 
-int convertStringToInt(string str) {
+/*
+ * Converts a string from the textfile to an integer.
+ */
+int convertStringToInt(const string str) {
     int integer;
     stringstream convert(str);
     convert >> integer;
     return integer;
 }
 
-void setGrid(Grid<char>& grid, ifstream& input, string& line, int& row_length,
-              int& col_length) {
+/*
+ * Sets the grid as the textfile dictates.
+ */
+void setGrid(Grid<char>& grid, ifstream& input, string& line, const int row_length,
+             const int col_length) {
     for(int row=0; row < row_length; row++) {
         getline(input, line);
         for(int col=0; col < col_length; col++) {
@@ -41,6 +49,11 @@ void setGrid(Grid<char>& grid, ifstream& input, string& line, int& row_length,
     }
 }
 
+/*
+ * Sets the right size of the grid from the first two rows
+ * of the textfile where the number of columns and rows of the grid is
+ * stated.
+ */
 void getGridFromFile(Grid<char>& grid) {
     string filename = askUserForFileName();
     ifstream input;
@@ -59,6 +72,9 @@ void getGridFromFile(Grid<char>& grid) {
     input.close();
 }
 
+/*
+ * Prints the grid as the same format as the textfile.
+ */
 void printGrid(const Grid<char>& grid) {
     for(int row = 0; row < grid.numRows(); row++) {
         for (int col = 0; col < grid.numCols(); col++) {
@@ -68,7 +84,10 @@ void printGrid(const Grid<char>& grid) {
     }
 }
 
-int checkTotalNeighbours(Grid<char>& grid, int row, int col) {
+/*
+ * Counts the amount of neighbours of a given cell.
+ */
+int checkTotalNeighbours(const Grid<char>& grid, const int row, const int col) {
     int totalNeighbours = 0;
 
     for (int inner_row = row-1; inner_row < row+2; inner_row++) {
@@ -88,6 +107,10 @@ int checkTotalNeighbours(Grid<char>& grid, int row, int col) {
     return totalNeighbours;
 }
 
+/*
+ * Ticks the cell simulation one step according to the rules of
+ * the game of life.
+ */
 void advanceGrid(Grid<char>& grid) {
     Grid<char> tempGrid(grid.numRows(), grid.numCols());
     int neighbours = 0;
@@ -116,6 +139,10 @@ void advanceGrid(Grid<char>& grid) {
     grid = tempGrid;
 }
 
+/*
+ * Ticks cell simulation one step forward every 100 ms while clearing the console
+ * and counting colony generations.
+ */
 void animate(Grid<char>& grid) {
     int generations = 0;
     while(1==1) {
@@ -128,6 +155,10 @@ void animate(Grid<char>& grid) {
     }
 }
 
+/*
+ * Checks user action input from the console and calls the appropriate
+ * functions.
+ */
 void userInterface(Grid<char>& grid) {
     string action = askUserForAction();
     if (action == "a") {
@@ -149,6 +180,9 @@ void userInterface(Grid<char>& grid) {
     }
 }
 
+/*
+ * Main function which starts the program.
+ */
 int main() {
     Grid<char> grid;
     getGridFromFile(grid);
