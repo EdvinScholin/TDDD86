b0720757582e815c725abf89b022156f910f0e53
edvsc779 <edvsc779@tlvm-5-3-1.ad.liu.se>
2021-10-13 17:31:40 +0200

    TDDD86 Lab 3 Redovisning

--- lab3/tsp/src/tsp.cpp
+++ lab3/tsp/src/tsp.cpp
@@ -16,77 +16,48 @@
 #include "Tour.h"
 
 int main(int argc, char *argv[]) {
-//    QApplication a(argc, argv);
+    QApplication a(argc, argv);
 
-//    string filename = "tsp1000.txt";
-//    ifstream input;
-//    input.open(filename);
+    string filename = "tsp100.txt";
+    ifstream input;
+    input.open(filename);
 
-//    // get dimensions
-//    int width;
-//    int height;
-//    input >> width;
-//    input >> height;
+    // get dimensions
+    int width;
+    int height;
+    input >> width;
+    input >> height;
 
-//    // setup graphical window
-//    QGraphicsView *view = new QGraphicsView();
-//    QGraphicsScene *scene = new QGraphicsScene();
-//    view->setScene(scene);
-//    view->scale(1, -1); //screen y-axis is inverted
-//    view->setSceneRect(0, 0, width, height);
-//    view->show();
+    // setup graphical window
+    QGraphicsView *view = new QGraphicsView();
+    QGraphicsScene *scene = new QGraphicsScene();
+    view->setScene(scene);
+    view->scale(1, -1); //screen y-axis is inverted
+    view->setSceneRect(0, 0, width, height);
+    view->show();
 
-//    // run insertion heuristic
-//    Tour tour;
-//    double x;
-//    double y;
-//    while (input >> x >> y) {
-//        Point p(x, y);
-//        tour.insertSmallest(p);
-//        //uncomment the 4 lines below to animate
-////        tour.draw(scene);
-////        std::chrono::milliseconds dura(50);
-////        std::this_thread::sleep_for(dura);
-////        a.processEvents();
-//    }
-//    input.close();
+    // run insertion heuristic
+    Tour tour;
+    double x;
+    double y;
+    while (input >> x >> y) {
+        Point p(x, y);
+        tour.insertNearest(p);
+        //uncomment the 4 lines below to animate
+//        tour.draw(scene);
+//        std::chrono::milliseconds dura(50);
+//        std::this_thread::sleep_for(dura);
+//        a.processEvents();
+    }
+    input.close();
 
-//    // print tour to standard output
-//    cout << "Tour distance: " << std::fixed << std::setprecision(4)
-//         << std::showpoint << tour.distance() << endl;
-//    cout << "Number of points: " << tour.size() << endl;
-//    tour.show();
+    // print tour to standard output
+    cout << "Tour distance: " << std::fixed << std::setprecision(4)
+         << std::showpoint << tour.distance() << endl;
+    cout << "Number of points: " << tour.size() << endl;
+    tour.show();
 
-//    // draw tour
-//    tour.draw(scene);
-//    return a.exec(); // start Qt event loop
-
-
-    // define 4 points forming a square
-    Point p(100.0, 100.0);
-    Point q(500.0, 100.0);
-    Point r(500.0, 500.0);
-    Point s(100.0, 500.0);
-    // Set up a Tour with those four points
-    // The constructor should link p->q->r->s->p
-    Tour squareTour(p, q, r, s);
-    // Output the Tour
-    Point c(150, 150);
-    squareTour.insertNearest(c);
-    squareTour.show();
-    // Output size of tour
-    squareTour.size();
-    // Output total distance of tour:
-    squareTour.distance();
-
-
-
-//    Point h(450.0, 450.0);
-//    squareTour.insertNearest(h);
-//    squareTour.show();
-//    squareTour.draw(scene);
-//    return a.exec(); // start Qt event loop
-
-    cout << endl;
-    cout << "\nPROGRAM DONE\n";
+    // draw tour
+    tour.draw(scene);
+    return a.exec(); // start Qt event loop
 }
