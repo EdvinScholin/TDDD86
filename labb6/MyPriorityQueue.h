85767079d943197fb1e80b6a1dde8f9cb1b7fba4
edvsc779 <edvsc779@su24-114.ad.liu.se>
2021-11-30 15:13:37 +0100

    Driver input problem?

--- labb6/MyPriorityQueue.h
+++ labb6/MyPriorityQueue.h
@@ -32,42 +32,75 @@ public:
 
 private:
     // Other private members?
+    int parent(int pos) {
+        if (pos <= 0) return -1;
+        return (pos-1)/2;
+      }
+
+    bool isLeaf(int pos) {
+        return (pos >= vector_array.size()/2) && (pos < vector_array.size());
+    }
+
+    int leftchild(int pos) {
+        if (pos >= vector_array.size()/2) return - 1;
+        return 2*pos + 1;
+      }
 };
 
 template <typename T, typename C>
 MyPriorityQueue<T,C>::MyPriorityQueue(){
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
 }
 
 template <typename T, typename C>
 MyPriorityQueue<T,C>::~MyPriorityQueue(){
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
 }
 
 template <typename T, typename C>
 void MyPriorityQueue<T,C>::push(const T& t){
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+    int currentPos = vector_array.size();
+    vector_array[currentPos] = t;
+    while ((currentPos != 0) && strictly_larger_operator(vector_array[parent(currentPos)], vector_array[currentPos])) {
+//        T temp = vector_array[parent(currentPos)]; // byt plats på parent och currentPos
+//        vector_array[parent(currentPos)] = vector_array[currentPos];
+//        vector_array[currentPos] = temp;
+        swap(vector_array[parent(currentPos)], vector_array[currentPos]);
+        currentPos = parent(currentPos); // Tror detta är rätt
+    }
 }
 
 template <typename T, typename C>
 T MyPriorityQueue<T,C>::top()const{
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+    return vector_array[0];
 }
 
 template <typename T, typename C>
 void MyPriorityQueue<T,C>::pop(){
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+    vector_array[0] = vector_array[vector_array.size() - 1];
+    vector_array.pop_back();
+
+    int currentPos = 0;
+    if (currentPos < 0 || currentPos > vector_array.size()) {
+        return;
+    }
+    while (!isLeaf(currentPos)) {
+       int child = leftchild(currentPos);
+       if ((child < vector_array.size() - 1) && strictly_larger_operator(vector_array[currentPos + 1], vector_array[currentPos])) { // which of the siblings is largest
+           child++; // becomes right child
+       }
+
+       if (strictly_larger_operator(vector_array[currentPos], vector_array[child])) {
+//           T temp = vector_array[child]; // byt plats på parent och currentPos
+//           vector_array[child] = vector_array[currentPos];
+//           vector_array[currentPos] = temp;
+           swap(vector_array[child], vector_array[currentPos]);
+           currentPos = child; // Tror detta är rätt
+       }
+    }
 }
 
 template <typename T, typename C>
 bool MyPriorityQueue<T,C>::empty()const{
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+    return vector_array.empty();
 }
 
 #endif // MY_PRIORITY_QUEUE_H
--- labb6/MyVector.h
+++ labb6/MyVector.h
@@ -14,39 +14,81 @@ class MyVector
 {
 
 public:
+
     MyVector();
 
     ~MyVector();
 
+    /*
+     * Copy constructor which sets capacity and numberOfElements to
+     * other's and fills storage with other's elements.
+     */
     MyVector(const MyVector& other);
 
+    /*
+     * Equal operator used in order to set a vector to another vector.
+     */
     MyVector& operator =(const MyVector& other);
 
-
+    /*
+     * Appends element to the vector. If capacity is equal to numberOfElements
+     * the method doubles storage's capacity
+     */
     void push_back(const T&);
 
+    /*
+     * Removes the last element in the vector.
+     */
     void pop_back();
 
+    /*
+     * Returns reference to element at the given index.
+     */
     T& operator[](unsigned i);
 
+    /*
+     * Returns const reference to element at the given index.
+     */
     const T& operator[](unsigned i)const;
 
     void remove(unsigned i);
 
+    /*
+     * Returns whether the vector is empty or not.
+     */
     bool empty()const;
 
+    /*
+     * Returns pointer to the beginning of the vector.
+     */
     T* begin();
 
+    /*
+     * Returns pointer to the an element after the last
+     * element the vector.
+     */
     T* end();
 
+    /*
+     * Clears the vector of all containing elements.
+     */
     void clear();
 
+    /*
+     * Returns numberOfElements.
+     */
     unsigned size()const;
 
 private:
     T* storage;
     unsigned capacity;
     unsigned numberOfElements;
+
+    void setElemetents(const MyVector& other) {
+        for (int i = 0; i < other.numberOfElements; i++) {
+            storage[i] = other.storage[i];
+        }
+    }
 };
 
 template<typename T>
@@ -58,54 +100,45 @@ MyVector<T>::MyVector(){
 
 template<typename T>
 MyVector<T>::~MyVector(){
-    delete storage;
+    delete[] storage;
 }
 
 template<typename T>
 MyVector<T>::MyVector(const MyVector& other){
     storage = new T [other.capacity];
-    //copy(begin(other.storage), end(other.storage), begin(storage)); // WE think this right is, yes!
-    for (int i = 0; i < other.numberOfElements; i++) {
-        storage[i] = other.storage[i];
-    }
+    setElemetents(other);
     numberOfElements = other.numberOfElements;
     capacity = other.capacity;
 }
 
 template<typename T>
-MyVector<T>& MyVector<T>::operator =(const MyVector& other){
+MyVector<T>& MyVector<T>::operator=(const MyVector& other){
     if (this != &other) {
         delete storage;
         storage = new T [other.capacity];
         capacity = other.capacity;
         numberOfElements = other.numberOfElements;
-        //copy(begin(other.storage), end(other.storage), begin(storage));
-        for (int i = 0; i < other.numberOfElements; i++) {
-            storage[i] = other.storage[i];
-        }
+        setElemetents(other);
     }
 
-    return *this; // kanske rätt
+    return *this;
 }
 
 template<typename T>
 void MyVector<T>::push_back(const T& e){
     if (numberOfElements == capacity) {
         T* temp = new T[2*capacity];
-        //copy(begin(storage), end(storage), begin(temp));
         for (int i = 0; i < numberOfElements; i++) {
             temp[i] = storage[i];
         }
 
-        //delete storage;
-        storage = temp; //Osäker med hur man gör med pekare
-        delete[] temp;
+        delete[] storage;
+        storage = temp;
         capacity *= 2;
     }
 
     storage[numberOfElements] = e;
     numberOfElements += 1;
-
 }
 
 template<typename T>
@@ -130,7 +163,7 @@ bool MyVector<T>::empty()const{
 
 template<typename T>
 void MyVector<T>::clear(){
-    delete storage;
+    delete[] storage;
     storage = new T [1];
     numberOfElements = 0;
     capacity = 1;
@@ -143,12 +176,12 @@ unsigned MyVector<T>::size()const{
 
 template<typename T>
 T* MyVector<T>::begin(){
-    return &storage;
+    return &storage[0]; //&storage;
 }
 
 template<typename T>
 T* MyVector<T>::end(){
-    return &(storage + capacity);
+    return &storage[numberOfElements]; //numberOfElements eller capacity?
 }
 
 #endif // MY_VECTOR_H
