55fd7b7a2f0ca8aab99832b1aa7d12cf67ae83fb
edvsc779 <edvsc779@su03-108.ad.liu.se>
2021-12-09 20:05:51 +0100

    Astar funkar ej

--- lab8/trailblazer/src/trailblazer.cpp
+++ lab8/trailblazer/src/trailblazer.cpp
@@ -5,170 +5,250 @@
 
 #include "costs.h"
 #include "trailblazer.h"
+#include "pqueue.h"
 #include <stack>
-#include <set>
-#include <algorithm>
 #include <queue>
-#include <unordered_set>
+#include <limits>
+#include <map>
+#include <set>
+
 // TODO: include any other headers you need; remove this comment
 using namespace std;
 
-void DFS(BasicGraph& graph, Vertex* start, Vertex* end, stack<Vertex*>& stack, vector<Vertex*>& path) {
+bool DFS(BasicGraph& graph, Vertex* start, Vertex* end, stack<Vertex*> stack1, vector<Vertex*>& path) {
     start->visited = true;
-    stack.push(start);
+    start->setColor(GREEN);
+    stack1.push(start);
 
     if (start == end) {
-        Node* node;
-        while(!stack.empty()) {
-            node = stack.top();
-            node->setColor(GREEN);
-            stack.pop();
-            path.push_back(node); // kanske blir baklänges.
+        stack<Vertex*> new_stack;
+        while (!stack1.empty()) {      //output the elements of the stack as the solution
+            new_stack.push(stack1.top());
+            stack1.pop();
         }
-        return;
+        while (!new_stack.empty()) {
+            path.push_back(new_stack.top());
+            new_stack.pop();
+        }
+        return true;
     }
 
     for (Vertex *vertex : graph.getNeighbors(start)) {
         if (!vertex->visited) {
-            if (!vertex->visited) {
-                vertex->visited = true;
-                vertex->setColor(YELLOW);
-                DFS(graph, vertex, end, stack, path);
+            vertex->visited = true;
+            if (DFS(graph, vertex, end, stack1, path)) {
+                return true;
             }
-
-            else {
-                vertex->setColor(GRAY);
-            }
+            vertex->setColor(GRAY);
         }
     }
 
-    stack.pop();
-
+    stack1.pop();
+    return false;
 }
-vector<Vertex*> DFSV2(BasicGraph& graph, Vertex* start, Vertex* end) {
-    //TODO: byt variabelnamn och fixa färgerna.
-    queue<stack<Vertex*>> q;
-    stack<Vertex*> chain_stack;
-    chain_stack.push(start);
-    q.push(chain_stack);                        //create/add a stack containing {w1} to the queue
 
-    while (!q.empty()) {                        //the queue is not empty
-        chain_stack = q.front();
-        q.pop();                                //dequeue the partial-chain stack from the front of the queue
-        if (chain_stack.top() == end) {          //if the word at the top of the stack is the destinaction word
-            vector<Vertex*> path;
-            stack<Vertex*> new_stack;
-            while (!chain_stack.empty()) {      //output the elements of the stack as the solution
-                //chain_stack.top()->setColor(GREEN); //för att visa vägen atm
-                new_stack.push(chain_stack.top());
-                chain_stack.pop();
-            }
-            while(!new_stack.empty()) {
-                path.push_back(new_stack.top());
-                new_stack.pop();
-
-            }
-            return path;
-
-        }
-
-        else {
-            for (Vertex *neighbour : graph.getNeighbors(chain_stack.top())) {
-                if (!neighbour->visited) {   //if that neighbour word has not already been used in a ladder before
-                    stack<Vertex*> copy_stack = chain_stack;             //create a copy of the current chain stack
-                    neighbour->visited = true;                     //ensure neighbour word is not used again
-                    neighbour->setColor(GREEN);
-                    copy_stack.push(neighbour);                         //put the neighbour word at the top of the copy stack
-                    q.push(copy_stack);                                 //add the copy stack to the end of the queue
-                }
-                neighbour->setColor(GRAY);
-            }
-        }
-    }
-    return {};
-}
 vector<Node *> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
-//    graph.resetData();
-
-//    vector<Vertex*> path = {};
-//    stack<Vertex*> stack;
-//    Vertex* currentNode;
-//    start->visited = true;
-//    start->setColor(GREEN);
-//    stack.push(start);
-
-
-//    while (!stack.empty()) {
-
-
-
-//       currentNode = stack.top();
-//       stack.pop();
-
-
-//       if (currentNode == end) {
-//           while(!stack.empty()) {
-//               currentNode = stack.top();
-//               stack.pop();
-//               path.push_back(currentNode); // kanske blir baklänges.
-//           }
-//           //reverse(path.begin(), path.end());
-//           return path;
-//       }
-
-//       for (Vertex *vertex : graph.getNeighbors(currentNode)) {
-//           if (!vertex->visited) {
-//               stack.push(vertex);
-//               vertex->visited = true;
-//               vertex->setColor(YELLOW);
-//           }
-//       }
-//    }
-//    return path;
-
     graph.resetData();
     stack<Vertex*> stack;
     vector<Vertex*> path = {};
-
-    //DFS(graph, start, end, stack, path);
-    //DFS(graph, start, end, stack, path);
-
-    path = DFSV2(graph, start, end);
+    DFS(graph, start, end, stack, path);
     return path;
-
 }
 
 
+Vertex* BFS(BasicGraph& graph, Vertex* start, Vertex* end, queue<Vertex*> q) {
+    Vertex* currentVertex;
 
+    q.push(start);
+    start->visited = true;
+    start->setColor(YELLOW);
+
+    while (!q.empty()) {
+        currentVertex = q.front();
+        currentVertex->setColor(GREEN);
+        q.pop();
+
+        if (currentVertex == end) {
+            return currentVertex;
+        }
+
+        for (auto neighbour : graph.getNeighbors(currentVertex)) {
+            if (!neighbour->visited) {
+                neighbour->previous = currentVertex;
+                neighbour->setColor(YELLOW);
+                neighbour->visited = true;
+                q.push(neighbour);
+            }
+        }
+    }
+
+    return nullptr;
+}
 
 
 vector<Node *> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
-    vector<Vertex*> path;
+    graph.resetData();
+    queue<Vertex*> q;
+    vector<Vertex*> path = {};
+
+    Vertex* vertex = BFS(graph, start, end, q);
+    stack<Vertex*> vertexStack;
+
+    while(vertex != start) {
+        vertexStack.push(vertex);
+        vertex = vertex->previous;
+    }
+
+    vertexStack.push(start);
+
+    while(!vertexStack.empty()) {
+        path.push_back(vertexStack.top());
+        vertexStack.pop();
+    }
+
     return path;
+}
+
+Vertex* dijsktraHelp(BasicGraph& graph, Vertex* start, Vertex* end) {
+    PriorityQueue<Vertex*> pq;
+    Vertex* bestVertex;
+    double altCost;
+
+    for (Vertex* vertex : graph.getNodeSet()) {
+        if (vertex != start) {
+            vertex->cost = numeric_limits<double>::infinity();
+            vertex->previous = nullptr;
+        }
+        pq.enqueue(vertex, vertex->cost);
+    }
+
+    while (!pq.isEmpty()) {
+        bestVertex = pq.dequeue();
+        bestVertex->setColor(GREEN);
+
+        if(bestVertex == end) {
+            return bestVertex;
+        }
+
+        for (Vertex* neighbour : graph.getNeighbors(bestVertex)) {
+            altCost = bestVertex->cost + graph.getEdge(bestVertex, neighbour)->cost;
+
+            if (altCost < neighbour->cost) {
+                neighbour->cost = altCost;
+                neighbour->previous = bestVertex;
+                pq.changePriority(neighbour, altCost);
+                neighbour->setColor(YELLOW);
+            }
+        }
+    }
 }
 
 vector<Node *> dijkstrasAlgorithm(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
-    vector<Vertex*> path;
+    graph.resetData();
+
+    vector<Vertex*> path {};
+
+    Vertex* vertex = dijsktraHelp(graph, start, end);
+    stack<Vertex*> vertexStack;
+
+    if (vertex == nullptr) { //kanske
+        return path;
+    }
+
+    while(vertex != start) {
+        vertexStack.push(vertex);
+        vertex = vertex->previous;
+    }
+
+    vertexStack.push(start);
+
+    while(!vertexStack.empty()) {
+        path.push_back(vertexStack.top());
+        vertexStack.pop();
+    }
+
+
     return path;
 }
 
+Vertex* aStarHelp(BasicGraph& graph, Vertex* start, Vertex* end) {
+    PriorityQueue<Vertex*> pq;
+    Vertex* bestVertex;
+    double altCost;
+    double bestGuessCost;
+    map<Vertex*, double> fscore; //fett onödig
+    set<Vertex*> openSet;
+
+
+    for (Vertex* vertex : graph.getNodeSet()) {
+        if (vertex != start) {
+            vertex->cost = numeric_limits<double>::infinity();
+            vertex->previous = nullptr;
+        }
+//        pq.enqueue(vertex, vertex->cost);
+//        openSet.insert(vertex);
+    }
+
+    pq.enqueue(start, start->cost);
+    openSet.insert(start);
+
+
+    while (!pq.isEmpty()) {
+        bestVertex = pq.dequeue();
+        openSet.erase(bestVertex);
+        bestVertex->setColor(GREEN);
+
+        if(bestVertex == end) {
+            return bestVertex;
+        }
+
+        for (Vertex* neighbour : graph.getNeighbors(bestVertex)) {
+            altCost = bestVertex->cost + graph.getEdge(bestVertex, neighbour)->cost;
+
+
+            if (altCost < neighbour->cost) {
+                neighbour->cost = altCost;
+                neighbour->previous = bestVertex;
+
+                bestGuessCost = altCost + bestVertex->heuristic(neighbour);
+
+
+                if(openSet.find(neighbour) == openSet.end()){
+                    pq.enqueue(neighbour, bestGuessCost);
+                    neighbour->setColor(YELLOW);
+                }
+
+
+
+            }
+        }
+    }
+}
+
 vector<Node *> aStar(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
-    vector<Vertex*> path;
+    graph.resetData();
+
+    vector<Vertex*> path {};
+
+    Vertex* vertex = aStarHelp(graph, start, end);
+    stack<Vertex*> vertexStack;
+
+    if (vertex == nullptr) { //kanske
+        return path;
+    }
+
+    while(vertex != start) {
+        vertexStack.push(vertex);
+        vertex = vertex->previous;
+    }
+
+    vertexStack.push(start);
+
+    while(!vertexStack.empty()) {
+        path.push_back(vertexStack.top());
+        vertexStack.pop();
+    }
+
+
     return path;
 }
