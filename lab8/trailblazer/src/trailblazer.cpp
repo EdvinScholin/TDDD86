86384c7413aaf836e00abb4f7393f9252f03c5cb
edvsc779 <edvsc779@su02-109.ad.liu.se>
2021-12-13 10:48:42 +0100

    TDDD86 Lab 8 redovisning

--- lab8/trailblazer/src/trailblazer.cpp
+++ lab8/trailblazer/src/trailblazer.cpp
@@ -1,7 +1,7 @@
-// This is the CPP file you will edit and turn in.
-// Also remove these comments here and add your own, along with
-// comments on every function and on complex code sections.
-// TODO: write comment header for this file; remove this comment
+/*
+ * This assignment was made by Edvin Schölin (edvsc779) and Wilmer Segerstedt (wilse150).
+ * The file contains four implementations of various search algorithms in a graph.
+ */
 
 #include "costs.h"
 #include "trailblazer.h"
@@ -9,135 +9,157 @@
 #include <stack>
 #include <queue>
 #include <limits>
-#include <map>
-#include <set>
 
-// TODO: include any other headers you need; remove this comment
 using namespace std;
 
-bool DFS(BasicGraph& graph, Vertex* start, Vertex* end, stack<Vertex*> stack1, vector<Vertex*>& path) {
+/*
+ * Backtracks from end vertex to start vertex.
+ */
+vector<Node*> makePath(Vertex* vertex, Vertex* start) {
+    vector<Vertex*> path = {};
+    stack<Vertex*> vertexStack;
+
+    if (vertex == nullptr) {
+        return path;
+    }
+
+    while (vertex != start) {
+        vertexStack.push(vertex);
+        vertex = vertex->previous;
+    }
+
+    vertexStack.push(start);
+
+    while (!vertexStack.empty()) { // This flips the order of the path since the stack contains the reverse order of the path.
+        path.push_back(vertexStack.top());
+        vertexStack.pop();
+    }
+
+    return path;
+}
+
+/*
+ * Helper function which contains algorithm for depth first search.
+ */
+bool dfsHelper(BasicGraph& graph, Vertex* start, Vertex* end, stack<Vertex*> stack1, vector<Vertex*>& path) {
     start->visited = true;
     start->setColor(GREEN);
-    stack1.push(start);
+    stack1.push(start); // Add start node to stack
 
     if (start == end) {
         stack<Vertex*> new_stack;
-        while (!stack1.empty()) {      //output the elements of the stack as the solution
+        while (!stack1.empty()) {
             new_stack.push(stack1.top());
             stack1.pop();
         }
-        while (!new_stack.empty()) {
+        while (!new_stack.empty()) { // Flips the order of the path
             path.push_back(new_stack.top());
             new_stack.pop();
         }
         return true;
     }
 
-    for (Vertex *vertex : graph.getNeighbors(start)) {
+    for (Vertex *vertex : graph.getNeighbors(start)) { // Iterates over all neighbours to given node
         if (!vertex->visited) {
             vertex->visited = true;
-            if (DFS(graph, vertex, end, stack1, path)) {
-                return true;
+
+            if (dfsHelper(graph, vertex, end, stack1, path)) {
+                return true; // We have found a path
             }
-            vertex->setColor(GRAY);
+            vertex->setColor(GRAY); // Vertex has been checked but not chosen in the final path.
         }
     }
 
-    stack1.pop();
+    stack1.pop(); // The node didn't lead anywhere
     return false;
 }
 
+/*
+ * Main function for depth first search.
+ */
 vector<Node *> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
     graph.resetData();
     stack<Vertex*> stack;
     vector<Vertex*> path = {};
-    DFS(graph, start, end, stack, path);
+    dfsHelper(graph, start, end, stack, path);
     return path;
 }
 
-
-Vertex* BFS(BasicGraph& graph, Vertex* start, Vertex* end, queue<Vertex*> q) {
+/*
+ * Helper function which contains algorithm for breadth first search.
+ */
+Vertex* bfsHelper(BasicGraph& graph, Vertex* start, Vertex* end, queue<Vertex*> q) {
     Vertex* currentVertex;
 
     q.push(start);
     start->visited = true;
-    start->setColor(YELLOW);
+    start->setColor(YELLOW); // In process
 
     while (!q.empty()) {
         currentVertex = q.front();
-        currentVertex->setColor(GREEN);
+        currentVertex->setColor(GREEN); // Is path of the final path
         q.pop();
 
-        if (currentVertex == end) {
+        if (currentVertex == end) { // We have found the end node and it will backtrack the path trough node->previous
             return currentVertex;
         }
 
-        for (Vertex* neighbour : graph.getNeighbors(currentVertex)) {
+        for (Vertex* neighbour : graph.getNeighbors(currentVertex)) { // Iterates over all neighbours to given node
             if (!neighbour->visited) {
-                neighbour->previous = currentVertex;
-                neighbour->setColor(YELLOW);
+                neighbour->previous = currentVertex; // This is what tracks the path back to the start node
+                neighbour->setColor(YELLOW); // In process
                 neighbour->visited = true;
                 q.push(neighbour);
             }
         }
     }
-
     return nullptr;
 }
 
-
+/*
+ * Main function for breadth first search
+ */
 vector<Node *> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
     graph.resetData();
     queue<Vertex*> q;
-    vector<Vertex*> path = {};
+    Vertex* vertex = bfsHelper(graph, start, end, q);
 
-    Vertex* vertex = BFS(graph, start, end, q);
-    stack<Vertex*> vertexStack;
-
-    while(vertex != start) {
-        vertexStack.push(vertex);
-        vertex = vertex->previous;
-    }
-
-    vertexStack.push(start);
-
-    while(!vertexStack.empty()) {
-        path.push_back(vertexStack.top());
-        vertexStack.pop();
-    }
-
-    return path;
+    return makePath(vertex, start);
 }
 
-Vertex* dijsktraHelp(BasicGraph& graph, Vertex* start, Vertex* end) {
+/*
+ * Helper function which contains algorithm for Djisktra search.
+ */
+Vertex* dijsktraHelper(BasicGraph& graph, Vertex* start, Vertex* end) {
     PriorityQueue<Vertex*> pq;
     Vertex* current;
     double altCost;
     double inf = numeric_limits<double>::infinity();
 
-    for (Vertex* vertex : graph.getNodeSet()) {
+    for (Vertex* vertex : graph.getNodeSet()) { // Set all node inital costs to infinty
         if (vertex != start) {
             vertex->cost = inf;
         }
     }
+
     pq.enqueue(start, start->cost);
     start->setColor(YELLOW);
 
     while (!pq.isEmpty()) {
-        current = pq.dequeue();
+        current = pq.dequeue(); // We want to check the node with highest priority (lowest cost)
         current->setColor(GREEN);
 
-        if(current == end) {
+        if (current == end) { // We have found the end node and it will backtrack the path trough node->previous
             return current;
         }
 
         for (Vertex* neighbour : graph.getNeighbors(current)) {
-            altCost = current->cost + graph.getEdge(current, neighbour)->cost;
+            altCost = current->cost + graph.getEdge(current, neighbour)->cost; // Calculate cost from start to neighbour
 
-            if (altCost < neighbour->cost) {
-                neighbour->cost = altCost;
-                neighbour->previous = current;
-                pq.enqueue(neighbour, altCost);
+            if (altCost < neighbour->cost) { // Which neighbour has the cheapest route?
+                neighbour->cost = altCost; // The new cost of the neighbour
+                neighbour->previous = current; // This is what tracks the path back to the start node
+                pq.enqueue(neighbour, altCost); // Add neighbour with the accurate cost to the prio queue
                 neighbour->setColor(YELLOW);
             }
         }
@@ -145,34 +167,19 @@ Vertex* dijsktraHelp(BasicGraph& graph, Vertex* start, Vertex* end) {
     return nullptr;
 }
 
+/*
+ * Main function for Djisktra algorithm.
+ */
 vector<Node *> dijkstrasAlgorithm(BasicGraph& graph, Vertex* start, Vertex* end) {
     graph.resetData();
+    Vertex* vertex = dijsktraHelper(graph, start, end);
 
-    vector<Vertex*> path {};
-
-    Vertex* vertex = dijsktraHelp(graph, start, end);
-    stack<Vertex*> vertexStack;
-
-    if (vertex == nullptr) { //kanske
-        return path;
-    }
-
-    while(vertex != start) {
-        vertexStack.push(vertex);
-        vertex = vertex->previous;
-    }
-
-    vertexStack.push(start);
-
-    while(!vertexStack.empty()) {
-        path.push_back(vertexStack.top());
-        vertexStack.pop();
-    }
-
-
-    return path;
+    return makePath(vertex, start);
 }
 
+/*
+ * Helper function which contains algorithm for aStar search.
+ */
 Vertex* aStarHelp(BasicGraph& graph, Vertex* start, Vertex* end) {
     Vertex* current;
     double altCost;
@@ -180,33 +187,33 @@ Vertex* aStarHelp(BasicGraph& graph, Vertex* start, Vertex* end) {
     double inf = numeric_limits<double>::infinity();
     PriorityQueue<Vertex*> pq;
 
-    for (Vertex* vertex : graph.getNodeSet()) {
+    for (Vertex* vertex : graph.getNodeSet()) { // Set all node inital costs to infinty
         if (vertex != start) {
             vertex->cost = inf;
         }
     }
-    pq.enqueue(start, start->heuristic(end));
+    pq.enqueue(start, start->heuristic(end)); // Enqueues the start node with the estimated cost to reach the end
     start->setColor(YELLOW);
     start->visited = true;
 
     while (!pq.isEmpty()) {
-        current = pq.dequeue();
+        current = pq.dequeue(); // We want to check the node with highest priority (lowest estimated cost to reach the end)
         current->visited = false;
         current->setColor(GREEN);
 
-        if (current == end) {
+        if (current == end) { // We have found the end node and it will backtrack the path trough node->previous
             return current;
         }
 
         for (Vertex* neighbour : graph.getNeighbors(current)) {
-            altCost = current->cost + graph.getEdge(current, neighbour)->cost;
-            if (altCost < neighbour->cost) {
+            altCost = current->cost + graph.getEdge(current, neighbour)->cost; // Calculate cost from start to neighbour
+            if (altCost < neighbour->cost) { // Which neighbour has the cheapest route?
                 neighbour->previous = current;
                 neighbour->cost = altCost;
-                bestGuess = altCost + neighbour->heuristic(end);
-
-                if (!neighbour->visited) {
-                    pq.enqueue(neighbour, bestGuess);
+                bestGuess = altCost + neighbour->heuristic(end); // Current best guess of the cheapest path cost
+                                                                 // from start to end
+                if (!neighbour->visited) { // Is node already in prio queue? If not, add it
+                    pq.enqueue(neighbour, bestGuess); // Enqueue node with the best estimated cost
                     neighbour->setColor(YELLOW);
                     neighbour->visited = true;
                 }
@@ -217,30 +224,12 @@ Vertex* aStarHelp(BasicGraph& graph, Vertex* start, Vertex* end) {
     return nullptr;
 }
 
+/*
+ * Main function for aStar search.
+ */
 vector<Node *> aStar(BasicGraph& graph, Vertex* start, Vertex* end) {
     graph.resetData();
-
-    vector<Vertex*> path {};
-
     Vertex* vertex = aStarHelp(graph, start, end);
-    stack<Vertex*> vertexStack;
 
-    if (vertex == nullptr) { //kanske
-        return path;
-    }
-
-    while(vertex != start) {
-        vertexStack.push(vertex);
-        vertex = vertex->previous;
-    }
-
-    vertexStack.push(start);
-
-    while(!vertexStack.empty()) {
-        path.push_back(vertexStack.top());
-        vertexStack.pop();
-    }
-
-
-    return path;
+    return makePath(vertex, start);
 }
